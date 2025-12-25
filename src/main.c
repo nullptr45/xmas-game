a7a9481c90e3589d91cf4a99eeb6da9139b72064
Nicolas Papirakis <whitercactus927@gmail.com>
2025-12-25 12:11:04 -0800

    blank window

--- src/main.c
+++ src/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <raylib.h>
+
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
 
 int main()
 {
-    puts("Hello world");
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "game");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
 }
